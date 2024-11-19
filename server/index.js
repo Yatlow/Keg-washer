2d29c6e668cf1b14f6c8b81724210cef98b73724
Yatlow <yisrael@atlow.co.il>
2024-11-19 12:16:33 +0200

    all good!

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ const allowedOrigins = [
     'https://yatlow.github.io',
     'http://localhost:5500',
     'http://localhost',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1'
 ];
 
 const corsOptions = {
